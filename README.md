9afe9a71fae6924b790ff671765c676d37be6e3a
hungnt <thaihung38@gmail.com>
2020-09-07 06:59:52 +0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # magento2-core
 Core for all magento2 projects
+
+# Copyright
+This project is copyright by VinhCD Co,Ltd.
+
+# Contact
+vinhcd.co.ltd@gmail.com
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright 2020 VinhCD Co.Ltd. or its affiliates. All Rights Reserved.
+ *
+ * Please contact vinhcd.co.ltd@gmail.com for more information
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vinhcd_Core',
+    __DIR__
+);
